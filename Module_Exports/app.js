be6cb6d669f3d3ca5c44cacc704a7bcdfb349dbf
aman rathore <aman@kali.aman>
2020-08-12 03:01:29 -0400

    Update

--- Module_Exports/app.js
+++ Module_Exports/app.js
@@ -5,5 +5,5 @@ var stuff = require('./stuff');
 console.log(stuff.counter(['aman','Rahul','Abhishek']));
 console.log(stuff.add(2,3));
 console.log(stuff.sub(3,2));
-console.log("The PI value is ",stuff.pi);
+console.log(stuff.add(stuff.pi,5));
 
--- Module_Exports/stuff.js
+++ Module_Exports/stuff.js
@@ -1,15 +1,25 @@
-const counter = function(arr){
+var counter = function(arr){
 	return 'The length of array is '+arr.length+' ';
 }
-const add = function(a,b){
+var add = function(a,b){
 	return `The Sum of two number is ${a+b}`;
 }
-const sub = function(a,b){
+var sub = function(a,b){
 	return `The Substraction of two number is ${a-b}`;
 }
 //Defining a PI
 var pi = 3.142;
+
+module.exports = {
+	counter: counter,
+	add: add,
+	sub: sub,
+	pi: pi
+};
+
+/*
 module.exports.counter = counter;
 module.exports.add = add;
 module.exports.sub = sub;
 module.exports.pi = pi;
+*/
