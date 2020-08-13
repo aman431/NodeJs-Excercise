0a4bd4ea1da03daf6499dfc875a90ecba8c835ae
aman rathore <amanrathod777@gmail.com>
2020-08-13 18:57:23 +0530

    Read and Write

--- File_Read_Write/app.js
+++ File_Read_Write/app.js
@@ -1,4 +1,10 @@
 let fs = require('fs');
+// Require A Fs Library
 let r = fs.readFileSync('Read.txt','utf8');
-let w = fs.writeFileSync('write.txt',r,'utf8');
-let a = fs.appendFileSync('write.txt','Some Extra Text','utf8');
+//Read a file 
+fs.writeFileSync('write.txt',r,'utf8');
+//Write a file 
+fs.appendFileSync('write.txt','Some Extra Text','utf8');
+//Apending/Updating a file 
+fs.renameSync('write.txt','Write.txt','utf8');
+//Rename a File 
