abfd599f0d08aa163364ff94c910b38068687b69
aman rathore <aman@kali.aman>
2020-08-13 12:52:45 -0400

    Update

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,7 @@
 var http = require('http');
-//console.log(http);
-var server = http.createServer(function(req,res){
+
+var server = http.createServer(function(req,res){ //Creating a server by using createServer
+	console.log("request has made"+req.url); 
 	res.writeHead(200,{'Content-Type':'text/plain'}); //Setting a Status 200 with content-type
 	res.end('Hey aman you calling me !!'); //end by calling a message
 });
